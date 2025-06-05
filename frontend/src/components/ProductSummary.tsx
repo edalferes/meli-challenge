1998447559faa829aaf81fd3ec3791c70cbe51c2
Edmilson Alferes <edmilson.alferes@alpheres.com>
2025-06-05 08:30:10 -0300

    feat: add images and more products

--- frontend/src/components/ProductSummary.tsx
+++ frontend/src/components/ProductSummary.tsx
@@ -20,7 +20,7 @@ export default function ProductSummary({ title, ratings, ratingsCount }: Product
         <div className="flex items-center space-x-2">
           <div className="flex text-yellow-400 text-xl">
             {'★'.repeat(Math.round(ratings))}
-            {'☆'.repeat(5 - Math.round(ratings))}
+            {'☆'.repeat(9 - Math.round(ratings))}
           </div>
           <span className="text-sm text-gray-600">({ratingsCount})</span>
         </div>
